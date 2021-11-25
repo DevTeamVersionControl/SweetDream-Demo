f1e2cbfa1ff7afc9f8b995c5509b9dae07cc8963
kcnq <2135433@cegeplimoilou.ca>
2021-11-25 11:39:52 -0500

    Reorganising GitHub structure

--- Prototypes/Popping Candy/Scripts/LevelSwitch.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-export var next_level:PackedScene
-
-
-func _on_LevelSwitch_body_entered(body):
-	if body.is_in_group("player"):
-		if get_tree().change_scene_to(next_level):
-			print("Error loading scene")
--- learning/UIScene/sayHello.gd
+++
@@ -1,7 +0,0 @@
-extends Panel
-
-func _ready():
-	get_node("Button").connect("pressed", self, "_on_Button_Pressed")
-	
-func _on_Button_Pressed():
-	get_node("Label").text = "HELLO!"
