c2cc7e6c0832b2277938ddf20ff6017c980cbf3b
kcnq <2135433@cegeplimoilou.ca>
2022-01-02 21:50:21 -0500

    Small animation adjustments

--- Base/Scripts/Enemy.gd
+++ Base/Scripts/Enemy.gd
@@ -5,7 +5,7 @@ const PIXELS_PER_METER = 16
 var motion = Vector2()
 var target
 var is_on_floor:bool
-export var speed = 5
+export var jump_lenght = 5
 export var jump_height = 8
 export var hp = 10
 
@@ -20,8 +20,12 @@ func pulse():
 	$Sprite.animation = "Jump"
 	yield(get_tree().create_timer(0.5), "timeout")
 	motion = target.global_position - global_position
-	motion.x = motion.normalized().x * speed
+	motion.x = motion.normalized().x * jump_lenght
 	motion.y = -jump_height
+	if motion.x > 0:
+		scale.x = -1
+	else:
+		scale.x = 1
 	if (global_position.y > get_parent().screen_size.y || global_position.x > get_parent().screen_size.x || global_position.x < 0):
 		queue_free()
 	yield($Sprite, "animation_finished")
