1bd6b53b4ff3579abf6fad21613bcc63f810e6c5
kcnq <2135433@cegeplimoilou.ca>
2022-02-19 22:09:34 -0500

    Reorganised the script folder, added logic and a button you have to hold down
    bug with the new button when both the jello cube and the player hold the button down

--- Base/Scripts/Environment/Triggers/Button.gd
+++ Base/Scripts/Environment/Triggers/Button.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 signal on
+signal off
 
 var on := false
 
@@ -9,3 +10,10 @@ func _on_Area2D_body_entered(body):
 		emit_signal("on")
 		on = true
 		$Sprite.frame = 1
+
+
+func _on_Button_body_exited(body):
+	if body.is_in_group("player") || body.is_in_group("movable"):
+		emit_signal("off")
+		on = false
+		$Sprite.frame = 0
