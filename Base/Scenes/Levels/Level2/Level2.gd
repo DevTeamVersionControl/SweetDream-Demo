b9590021e7e4c2bf4bdf0435cdfbe677d180f36e
kcnq <2135433@cegeplimoilou.ca>
2022-04-16 22:12:21 -0400

    Started working on the tilesets

--- Base/Scenes/Levels/Level2/Level2.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-var mob = preload("res://Scenes/NPCs/Enemies/JelloEnemy.tscn")
-
-func _on_MobTimer_timeout():
-	var new_mob = mob.instance()
-	add_child(new_mob)
-	new_mob.global_position = $Position2D.global_position
-	new_mob.target = $Player
