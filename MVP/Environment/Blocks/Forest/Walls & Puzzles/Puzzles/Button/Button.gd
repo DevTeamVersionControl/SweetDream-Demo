40bb9892ec71326ec25e339e528489bdc33e12b4
AMD <2135433@cegeplimoilou.ca>
2022-12-24 17:04:32 -0500

    Added buttons and re exported the art for the puzzles

---
+++ MVP/Environment/Blocks/Forest/Walls & Puzzles/Puzzles/Button/Button.gd
@@ -0,0 +1,34 @@
+tool
+extends Area2D
+
+signal on
+signal off
+
+export var colour:int setget set_colour
+
+var bodies := 0
+var on := false
+
+func _on_Area2D_body_entered(body):
+	if body.is_in_group("pushbutton"):
+		if bodies < 0:
+			bodies = 0
+		bodies += 1
+		emit_signal("on")
+		on = true
+		$Sprite.frame = 1 + colour * 2
+		$Sprite.position.y = 0
+
+func _on_Area2D_body_exited(body):
+	if body.is_in_group("pushbutton"):
+		bodies -= 1
+		if bodies < 1:
+			emit_signal("off")
+			on = false
+			$Sprite.frame = 0 + colour * 2
+			$Sprite.position.y = -3
+
+func set_colour(new_colour):
+	colour = new_colour
+	$Sprite.frame = 0 + colour * 2
+	$Sprite.position.y = -3
---
+++ MVP/Environment/Blocks/Forest/Walls & Puzzles/Puzzles/Button/SpringlessButton.gd
@@ -0,0 +1,24 @@
+tool
+extends Area2D
+
+signal on
+
+export var colour:int setget set_colour
+
+var bodies := 0
+var on := false
+
+func _on_Area2D_body_entered(body):
+	if body.is_in_group("pushbutton"):
+		if bodies < 0:
+			bodies = 0
+		bodies += 1
+		emit_signal("on")
+		on = true
+		$Sprite.frame = 1 + colour * 2
+		$Sprite.position.y = 0
+
+func set_colour(new_colour):
+	colour = new_colour
+	$Sprite.frame = 0 + colour * 2
+	$Sprite.position.y = -3
